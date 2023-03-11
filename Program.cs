18570f34a837c51faa9eb9fd7ef3b9be150e4eac
Aruzhan <aruzhan-manarbekova@mail.ru>
2023-03-12 01:04:41 +0600

    Решение задачи №6

--- Program.cs
+++ Program.cs
@@ -1,24 +1,16 @@
-﻿// Задача 4: Напишите программу, которая принимает на вход три числа 
-// и выдаёт максимальное из этих чисел.
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
 
 Console.Clear();
 Console.Write("Введите 1 число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2 число: ");
-int n2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 3 число: ");
-int n3 = Convert.ToInt32(Console.ReadLine());
 
-int maxi = n;
-
-if (n2 > maxi)
-    maxi = n2;
-if (n3 > maxi)
-   maxi = n3;
-
-Console.WriteLine($"max = {maxi}");
+if (n % 2 == 0)
+    Console.WriteLine($"четное число");
+else
+    Console.WriteLine($"нечетное число");
 
